189cc41603285b9358d7083c166017d454b1aa33
crissFaDev <cr9211999@gmail.com>
2024-06-03 11:06:37 -0500

    v1 End Ecommerce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path: 'home',
+    component: HomePageComponent,
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
